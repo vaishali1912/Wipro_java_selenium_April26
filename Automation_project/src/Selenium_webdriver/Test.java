71466d3f039ebbfece1e0558ae6a02da5f8d4abb
vaishali-secondaccount <vaishali.mindstoneedutech@gmail.com>
2026-04-20 15:24:38 +0530

    first java code

---
+++ Automation_project/src/Selenium_webdriver/Test.java
@@ -0,0 +1,16 @@
+package Selenium_webdriver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+
+public class Test {
+
+	public static void main(String[] args) {
+		WebDriver driver=new ChromeDriver();
+		driver.get("https://googlechromelabs.github.io/chrome-for-testing/#stable");
+		
+		
+
+	}
+
+}
---
+++ First_selenium/src/First_package/Firstclass.java
@@ -0,0 +1,11 @@
+package First_package;
+
+public class Firstclass {
+
+	public static void main(String[] args) {
+		System.out.println("HEllo dear Students");
+		
+		
+	}
+
+}
